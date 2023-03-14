b82ca775dfc941837420a6836d10b362e3f1813c
linshatsm <linsha@tsmc.com>
2023-03-14 15:58:46 +0800

    update kind-deploy.sh and chart values

--- kind-deploy.sh
+++ kind-deploy.sh
@@ -7,5 +7,11 @@ kubectl create ns dubbo-demo
 # helm install
 helm install dubbo-admin -f ./charts/admin-stack/values.yaml --namespace dubbo-demo ./charts/admin-stack
 
+# helm uninstall
+helm uninstall dubbo-admin --namespace dubbo-demo
+
 # port-forward
 kubectl -n dubbo-demo port-forward svc/dubbo-admin 8080:38080
+# kubectl -n dubbo-demo port-forward svc/zookeeper 2181:2181
+
+# helm uninstall
