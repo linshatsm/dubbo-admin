eebbe4675c464dc570ececf9439254a9471fcf73
linshatsm <linsha@tsmc.com>
2023-03-14 14:10:36 +0800

    update kind-deploy.sh with port-forward

--- kind-deploy.sh
+++ kind-deploy.sh
@@ -6,3 +6,6 @@ kubectl create ns dubbo-demo
 
 # helm install
 helm install dubbo-admin -f ./charts/admin-stack/values.yaml --namespace dubbo-demo ./charts/admin-stack
+
+# port-forward
+kubectl -n dubbo-demo port-forward svc/dubbo-admin 8080:38080
